8bae9acb7c5eea7be02e62f9eb751fdac48ddbf2
Sandesh Panthee <sangm3138@gmail.com>
2025-12-04 11:14:02 +0545

    day6 ex2

---
+++ week3-mathematics/day6/day6_exercise2.py
@@ -0,0 +1,20 @@
+# Perform a T-test
+
+# importing libraries
+from scipy.stats import ttest_ind
+
+# sample dataset
+group1 = [2.1, 2.5, 2.8, 3.0, 3.2]
+group2 = [1.8, 2.0, 2.4, 2.7, 2.9]
+
+# Perform t-test
+t_stat, p_value = ttest_ind(group1, group2)
+print("T-Statistic: ", t_stat)
+print("P-Value: ", p_value)
+
+# Interpretation
+alpha = 0.05
+if p_value < alpha:
+  print("Reject the null Hypothesis: Significant difference.")
+else:
+  print("Failed to reject the null hypothesis: No significant difference.")
