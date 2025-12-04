52ac9e25dd3a5bbe37240bce68f42530e9f5828d
Sandesh Panthee <sangm3138@gmail.com>
2025-12-04 11:08:24 +0545

    day6 ex1 basic stats

---
+++ week3-mathematics/day6/day6_exercise1.py
@@ -0,0 +1,16 @@
+# Calculate Mean, Variance and Standard Deviation
+
+# importing libraries
+import numpy as np
+
+# Dataset
+data = [10, 20, 30, 40, 50]
+
+# calculate stats
+mean = np.mean(data)
+variance = np.var(data)
+std_dev = np.std(data)
+
+print("Mean: ", mean)
+print("Variance: ", variance)
+print("Standard Deviation: ", std_dev)
