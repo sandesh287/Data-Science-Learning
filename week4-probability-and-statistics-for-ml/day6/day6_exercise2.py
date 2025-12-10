29a327d53042540cbabef360c73fce91f93372a8
Sandesh Panthee <sangm3138@gmail.com>
2025-12-10 17:11:09 +0545

    ex2 simple linear regression

---
+++ week4-probability-and-statistics-for-ml/day6/day6_exercise2.py
@@ -0,0 +1,27 @@
+# Fit a Simple Linear Regression Model
+
+# libraries
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+# Generate data
+np.random.seed(42)
+x = np.random.rand(100, 1) * 10
+y = 3 * x + np.random.randn(100, 1) * 2
+
+# Fit linear regression
+model = LinearRegression()
+model.fit(x, y)
+
+# Get Coefficients
+slope = model.coef_[0][0]
+intercept = model.intercept_[0]
+r_squared = model.score(x, y)
+
+# Visualize
+plt.scatter(x, y, color='blue', label='Data')
+plt.plot(x, model.predict(x), color='red', label='Regression Line')
+plt.title("Linear Regression")
+plt.legend()
+plt.show()
